f087ccf6e12f7b435e2c5c3ae0c5d358e0065b86
Tauhidul Islam <tauhidul@misfit.tech>
2019-07-24 00:17:52 +0600

    Create dynamic board

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Board from './app/Board'
 
 function App() {
-  return <Board size={5}/>;
+  return <Board size={10} mine={12}/>;
 }
 
 export default App;
--- src/app/Board.js
+++ src/app/Board.js
@@ -2,25 +2,60 @@ import React, {Component} from 'react';
 import Box from './Box'
 import BOX_VALUE from './BoxValue';
 
+function createBoard(boardSize, numberOfMines) {
+  let board = [];
+  for (let i = 0; i < boardSize; i++) {
+    board.push(Array(boardSize).fill(0));
+  }
+
+  for(let i = 0; i < numberOfMines;) {
+    let randRow = Math.floor(Math.random() * boardSize);
+    let randCol = Math.floor(Math.random() * boardSize);
+
+    if(board[randRow][randCol] === 0) {
+      board[randRow][randCol] = -1;
+      i++;
+    }
+  }
+
+  let i = 0;
+  for(let j = 0; j < boardSize; j++) {
+    if(board[i][j] !== -1) {
+      board[i][j] = [board[i][j-1], board[i][j+1], board[i+1][j-1], board[i+1][j], board[i+1][j+1]].filter(x => x === -1).length
+    }
+  }
+
+  for(i++; i < boardSize - 1; i++) {
+    for(let j = 0; j < boardSize; j++) {
+      if(board[i][j] !== -1) {
+        board[i][j] = [board[i-1][j-1], board[i-1][j], board[i-1][j+1], board[i][j-1], board[i][j+1],
+          board[i+1][j-1], board[i+1][j], board[i+1][j+1]].filter(x => x === -1).length
+      }
+    }
+  }
+
+  for(let j = 0; j < boardSize; j++) {
+    if(board[boardSize-1][j] !== -1) {
+      board[i][j] = [board[i-1][j-1], board[i-1][j], board[i-1][j+1], board[i][j-1], board[i][j+1]].filter(x => x === -1).length
+    }
+  }
+
+  return board;
+}
+
 class Board extends Component {
   constructor(props) {
     super(props);
 
-    this.realValues = [
-      [1,  1,  1,  1,  -1],
-      [1,  -1,  2,  2,  2],
-      [2,  2,  2,  -1,  1],
-      [-1,  2,  2,  1,  1],
-      [2,  -1,  1,  0,  0]
-    ];
+    this.realBoard = createBoard(props.size, props.mine);
 
-    let userInputs = [];
+    let myBoard = [];
     for (let i = 0; i < props.size; i++) {
-      userInputs.push(Array(props.size).fill(BOX_VALUE['hidden']));
+      myBoard.push(Array(props.size).fill(BOX_VALUE['hidden']));
     }
 
     this.state = {
-      userInputs: userInputs,
+      myBoard: myBoard,
       gameEnded: false,
       success: false
     };
@@ -30,50 +65,50 @@ class Board extends Component {
   }
 
   handleBoxLeftClick(row, col) {
-    if(this.realValues[row][col] === -1) {
+    if(this.realBoard[row][col] === -1) {
       this.setState({
         gameEnded: true,
         success: false
       });
     }
-    else if(this.realValues[row][col] === 0) {
+    else if(this.realBoard[row][col] === 0) {
       // ToDo: will open all boxes around it
     }
 
-    let userInputs = this.state.userInputs;
-    userInputs[row][col] = BOX_VALUE['open'];
-    this.setState({userInputs: userInputs});
+    let myBoard = this.state.myBoard;
+    myBoard[row][col] = BOX_VALUE['open'];
+    this.setState({myBoard: myBoard});
   }
 
   handleBoxRightClick(row, col) {
-    let userInputs = this.state.userInputs;
-    if(userInputs[row][col] === BOX_VALUE['hidden']) {
-      userInputs[row][col] = BOX_VALUE['flagged'];
+    let myBoard = this.state.myBoard;
+    if(myBoard[row][col] === BOX_VALUE['hidden']) {
+      myBoard[row][col] = BOX_VALUE['flagged'];
     }
-    else if(userInputs[row][col] === BOX_VALUE['flagged']) {
-      userInputs[row][col] = BOX_VALUE['marked'];
+    else if(myBoard[row][col] === BOX_VALUE['flagged']) {
+      myBoard[row][col] = BOX_VALUE['marked'];
     }
-    else if(userInputs[row][col] === BOX_VALUE['marked']) {
-      userInputs[row][col] = BOX_VALUE['hidden'];
+    else if(myBoard[row][col] === BOX_VALUE['marked']) {
+      myBoard[row][col] = BOX_VALUE['hidden'];
     }
-    else if(userInputs[row][col] === BOX_VALUE['open']) {
+    else if(myBoard[row][col] === BOX_VALUE['open']) {
       // ToDo: Will open all boxes around it if safe mode
     }
 
-    this.setState({userInputs: userInputs});
+    this.setState({myBoard: myBoard});
   }
 
   render() {
     let size = this.props.size;
-    const userInputs = this.state.userInputs;
+    const myBoard = this.state.myBoard;
     const board = [];
     for(let i = 0; i < size; i++) {
       for(let j = 0; j < size; j++) {
         board.push(
           <Box
             key={'row'+i+'-col'+j}
-            value={this.realValues[i][j]}
-            userInput={userInputs[i][j]}
+            value={this.realBoard[i][j]}
+            userInput={myBoard[i][j]}
             position={[i,j]}
             gameEnded={this.state.gameEnded}
             success={this.state.success}
