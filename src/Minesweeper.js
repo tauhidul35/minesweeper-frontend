49a10ca36b07a5a9e6578f41601c362dafa73947
Tauhidul Islam <tauhidul@misfit.tech>
2019-08-16 19:19:18 +0600

    Move all business logic to Parent component (Minesweeper)

--- src/Minesweeper.js
+++ src/Minesweeper.js
@@ -1,28 +1,182 @@
 import React, {Component} from 'react';
 import Board from './minesweeper/Board';
 import ControlPanel from './minesweeper/ControlPanel';
+import BOX_VALUE from "./minesweeper/BoxValue";
+
+const CreateBoard = (boardSize, numberOfMines) => {
+  let board = [];
+  for (let i = 0; i < boardSize; i++) {
+    board.push(Array(boardSize).fill(0));
+  }
+
+  for(let i = 0; i < numberOfMines;) {
+    let randRow = Math.floor(Math.random() * boardSize);
+    let randCol = Math.floor(Math.random() * boardSize);
+
+    if(board[randRow][randCol] === 0) {
+      board[randRow][randCol] = -1;
+      i++;
+    }
+  }
+
+  for(let i = 0; i < boardSize; i++) {
+    for(let j = 0; j < boardSize; j++) {
+      if(board[i][j] === -1) {
+        continue;
+      }
+
+      let adjacent = [board[i][j-1], board[i][j+1]];
+
+      if(i > 0) {
+        adjacent.push(...[board[i-1][j-1], board[i-1][j], board[i-1][j+1]]);
+      }
+
+      if(i < boardSize -1) {
+        adjacent.push(...[board[i+1][j-1], board[i+1][j], board[i+1][j+1]]);
+      }
+
+      board[i][j] = adjacent.filter(x => x === -1).length
+    }
+  }
+
+  return board;
+};
 
 class Minesweeper extends Component {
   constructor(props) {
     super(props);
+    
+    // Set default value
+    const defaultBoardSize = 10;
+    const defaultTotalMine = 12;
+
+    // Create empty user board
+    let userBoard = [];
+    for (let i = 0; i < defaultBoardSize; i++) {
+      userBoard.push(Array(defaultBoardSize).fill(BOX_VALUE['hidden']));
+    }
+
     this.state = {
-      gameRunning: false
+      gameEnded: false,
+      success: false,
+      boardSize: defaultBoardSize,
+      totalMine: defaultTotalMine,
+      userBoard: userBoard,
+      gameStarted: false
     };
-    this.handleGameRunning = this.handleGameRunning.bind(this);
+
+    // Create game board with random mines position
+    this.gameBoard = CreateBoard(this.state.boardSize, this.state.totalMine);
+
+    this.leftClickOnBox = this.leftClickOnBox.bind(this);
+    this.rightClickOnBox = this.rightClickOnBox.bind(this);
   }
 
-  handleGameRunning(status) {
-    this.setState({gameRunning: status});
+  openBox(row, col, openMine = true) {
+    let userBoard = this.state.userBoard;
+
+    if(userBoard[row][col] === BOX_VALUE['open'] || userBoard[row][col] === BOX_VALUE['flagged']) {
+      return false;
+    }
+
+    if(this.gameBoard[row][col] !== -1 || openMine) {
+      userBoard[row][col] = BOX_VALUE['open'];
+      this.setState({userBoard: userBoard});
+    }
+
+    if(this.gameBoard[row][col] === -1 && openMine) {
+      this.setState({
+        gameEnded: true,
+        success: false
+      });
+    }
+    else if(this.gameBoard[row][col] === 0) {
+      this.openAdjacentBoxes(row, col);
+    }
+  };
+
+  openAdjacentBoxes(row, col) {
+    const userBoard = this.state.userBoard;
+    const maxIndex = userBoard.length - 1;
+    let openMine = false;
+
+    // check if all flag is added
+    if(this.gameBoard[row][col] !== 0) {
+      let numberOfFlags = [userBoard[row][col - 1], userBoard[row][col + 1]].filter(value => value === BOX_VALUE['flagged']).length;
+
+      if (row > 0) {
+        numberOfFlags += [userBoard[row - 1][col - 1], userBoard[row - 1][col], userBoard[row - 1][col + 1]].filter(value => value === BOX_VALUE['flagged']).length;
+      }
+
+      if (row < maxIndex) {
+        numberOfFlags += [userBoard[row + 1][col - 1], userBoard[row + 1][col], userBoard[row + 1][col + 1]].filter(value => value === BOX_VALUE['flagged']).length;
+      }
+
+      if (numberOfFlags !== this.gameBoard[row][col]) {
+        return false;
+      }
+      openMine = true;
+    }
+
+    // open all adjacent boxes
+    if(row > 0 && col > 0) { this.openBox(row-1, col-1, openMine); }
+    if(row > 0) { this.openBox(row-1, col, openMine); }
+    if(row > 0 && col < maxIndex) { this.openBox(row-1, col+1, openMine); }
+    if(col > 0) { this.openBox(row, col-1, openMine); }
+    if(col < maxIndex) { this.openBox(row, col+1, openMine); }
+    if(row < maxIndex && col > 0) { this.openBox(row+1, col-1, openMine); }
+    if(row < maxIndex) { this.openBox(row+1, col, openMine); }
+    if(row < maxIndex && col < maxIndex) { this.openBox(row+1, col, openMine); }
+  };
+
+  leftClickOnBox(row, col) {
+    this.setState({gameStarted: true});
+
+    if(this.state.userBoard[row][col] === BOX_VALUE['open']) {
+      this.openAdjacentBoxes(row, col);
+    }
+    else {
+      this.openBox(row, col, true);
+    }
+  }
+
+  rightClickOnBox(row, col) {
+    this.setState({gameStarted: true});
+
+    let userBoard = this.state.userBoard;
+    if(userBoard[row][col] === BOX_VALUE['hidden']) {
+      userBoard[row][col] = BOX_VALUE['flagged'];
+    }
+    else if(userBoard[row][col] === BOX_VALUE['flagged']) {
+      userBoard[row][col] = BOX_VALUE['marked'];
+    }
+    else if(userBoard[row][col] === BOX_VALUE['marked']) {
+      userBoard[row][col] = BOX_VALUE['hidden'];
+    }
+    else if(userBoard[row][col] === BOX_VALUE['open']) {
+      this.openAdjacentBoxes(row, col);
+    }
+
+    this.setState({userBoard: userBoard});
   }
 
   render() {
     return (
       <div className='row'>
         <div className='col-8'>
-          <Board size={10} mine={12} handleGameRunning={this.handleGameRunning}/>
+          <Board
+            size={this.state.boardSize}
+            mine={this.state.totalMine}
+            gameBoard={this.gameBoard}
+            userBoard={this.state.userBoard}
+            gameEnded={this.state.gameEnded}
+            success={this.state.success}
+            leftClickOnBox={this.leftClickOnBox}
+            rightClickOnBox={this.rightClickOnBox}
+          />
         </div>
         <div className='col-4'>
-          <ControlPanel gameRunning={this.state.gameRunning}/>
+          <ControlPanel gameRunning={this.state.gameStarted && !this.state.gameEnded}/>
         </div>
       </div>
     );
--- src/minesweeper/Board.js
+++ src/minesweeper/Board.js
@@ -1,189 +1,36 @@
-import React, {Component} from 'react';
-import Box from './Box'
-import BOX_VALUE from './BoxValue';
+import React from 'react';
+import Box from './Box';
 
-function createBoard(boardSize, numberOfMines) {
-  let board = [];
-  for (let i = 0; i < boardSize; i++) {
-    board.push(Array(boardSize).fill(0));
-  }
-
-  for(let i = 0; i < numberOfMines;) {
-    let randRow = Math.floor(Math.random() * boardSize);
-    let randCol = Math.floor(Math.random() * boardSize);
-
-    if(board[randRow][randCol] === 0) {
-      board[randRow][randCol] = -1;
-      i++;
-    }
-  }
-
-  for(let i = 0; i < boardSize; i++) {
-    for(let j = 0; j < boardSize; j++) {
-      if(board[i][j] === -1) {
-        continue;
-      }
-
-      let adjacent = [board[i][j-1], board[i][j+1]];
-
-      if(i > 0) {
-        adjacent.push(...[board[i-1][j-1], board[i-1][j], board[i-1][j+1]]);
-      }
-
-      if(i < boardSize -1) {
-        adjacent.push(...[board[i+1][j-1], board[i+1][j], board[i+1][j+1]]);
-      }
-
-      board[i][j] = adjacent.filter(x => x === -1).length
-    }
-  }
-
-  return board;
-}
-
-class Board extends Component {
-  constructor(props) {
-    super(props);
-
-    this.realBoard = createBoard(props.size, props.mine);
-
-    let myBoard = [];
-    for (let i = 0; i < props.size; i++) {
-      myBoard.push(Array(props.size).fill(BOX_VALUE['hidden']));
-    }
-
-    this.state = {
-      myBoard: myBoard,
-      gameEnded: false,
-      success: false
-    };
-
-    this.handleBoxLeftClick = this.handleBoxLeftClick.bind(this);
-    this.handleBoxRightClick = this.handleBoxRightClick.bind(this);
-  }
-
-  openBox(row, col, openMine = true) {
-    let myBoard = this.state.myBoard;
-
-    if(myBoard[row][col] === BOX_VALUE['open'] || myBoard[row][col] === BOX_VALUE['flagged']) {
-      return false;
-    }
-
-    if(this.realBoard[row][col] !== -1 || openMine) {
-      myBoard[row][col] = BOX_VALUE['open'];
-      this.setState({myBoard: myBoard});
-    }
-
-    if(this.realBoard[row][col] === -1 && openMine) {
-      this.setState({
-        gameEnded: true,
-        success: false
-      });
-      this.props.handleGameRunning(false);
-    }
-    else if(this.realBoard[row][col] === 0) {
-      this.openAdjacentBoxes(row, col);
-    }
-  }
-
-  openAdjacentBoxes(row, col) {
-    const myBoard = this.state.myBoard;
-    const maxIndex = myBoard.length - 1;
-    let openMine = false;
-
-    // check if all flag is added
-    if(this.realBoard[row][col] !== 0) {
-      let numberOfFlags = [myBoard[row][col - 1], myBoard[row][col + 1]].filter(value => value === BOX_VALUE['flagged']).length;
-
-      if (row > 0) {
-        numberOfFlags += [myBoard[row - 1][col - 1], myBoard[row - 1][col], myBoard[row - 1][col + 1]].filter(value => value === BOX_VALUE['flagged']).length;
-      }
-
-      if (row < maxIndex) {
-        numberOfFlags += [myBoard[row + 1][col - 1], myBoard[row + 1][col], myBoard[row + 1][col + 1]].filter(value => value === BOX_VALUE['flagged']).length;
-      }
-
-      if (numberOfFlags !== this.realBoard[row][col]) {
-        return false;
-      }
-      openMine = true;
-    }
-
-    // open all adjacent boxes
-    if(row > 0 && col > 0) { this.openBox(row-1, col-1, openMine); }
-    if(row > 0) { this.openBox(row-1, col, openMine); }
-    if(row > 0 && col < maxIndex) { this.openBox(row-1, col+1, openMine); }
-    if(col > 0) { this.openBox(row, col-1, openMine); }
-    if(col < maxIndex) { this.openBox(row, col+1, openMine); }
-    if(row < maxIndex && col > 0) { this.openBox(row+1, col-1, openMine); }
-    if(row < maxIndex) { this.openBox(row+1, col, openMine); }
-    if(row < maxIndex && col < maxIndex) { this.openBox(row+1, col, openMine); }
-  }
-
-  handleBoxLeftClick(row, col) {
-    this.props.handleGameRunning(true);
-
-    if(this.state.myBoard[row][col] === BOX_VALUE['open']) {
-      this.openAdjacentBoxes(row, col);
-    }
-    else {
-      this.openBox(row, col, true);
-    }
-  }
-
-  handleBoxRightClick(row, col) {
-    this.props.handleGameRunning(true);
-
-    let myBoard = this.state.myBoard;
-    if(myBoard[row][col] === BOX_VALUE['hidden']) {
-      myBoard[row][col] = BOX_VALUE['flagged'];
-    }
-    else if(myBoard[row][col] === BOX_VALUE['flagged']) {
-      myBoard[row][col] = BOX_VALUE['marked'];
-    }
-    else if(myBoard[row][col] === BOX_VALUE['marked']) {
-      myBoard[row][col] = BOX_VALUE['hidden'];
-    }
-    else if(myBoard[row][col] === BOX_VALUE['open']) {
-      this.openAdjacentBoxes(row, col);
-    }
-
-    this.setState({myBoard: myBoard});
-  }
-
-  render() {
-    let size = this.props.size;
-    const myBoard = this.state.myBoard;
-    const board = [];
-    for(let i = 0; i < size; i++) {
-      for(let j = 0; j < size; j++) {
-        board.push(
-          <Box
-            key={'row'+i+'-col'+j}
-            value={this.realBoard[i][j]}
-            userInput={myBoard[i][j]}
-            position={[i,j]}
-            gameEnded={this.state.gameEnded}
-            success={this.state.success}
-            handleLeftClick={this.handleBoxLeftClick}
-            handleRightClick={this.handleBoxRightClick}
-          />
-        );
-      }
+function Board(props) {
+  const board = [];
+  for(let i = 0; i < props.size; i++) {
+    for(let j = 0; j < props.size; j++) {
       board.push(
-        <div
-          key={'row'+i+'-col'}
-          className='clearFix'
+        <Box
+          key={'row'+i+'-col'+j}
+          value={props.gameBoard[i][j]}
+          userInput={props.userBoard[i][j]}
+          position={[i,j]}
+          gameEnded={props.gameEnded}
+          success={props.success}
+          handleLeftClick={props.leftClickOnBox}
+          handleRightClick={props.rightClickOnBox}
         />
       );
     }
-
-    return (
-      <div className='board' style={{width: `${size*50+2}px`}}>
-        {board}
-      </div>
-    )
+    board.push(
+      <div
+        key={'row'+i+'-col'}
+        className='clearFix'
+      />
+    );
   }
+
+  return (
+    <div className='board' style={{width: `${props.size*50+2}px`}}>
+      {board}
+    </div>
+  )
 }
 
 export default Board;
